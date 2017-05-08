b4f17c2358ae0ac3a578378a0da53221816ec431
Joachim Bakke <github@heiamoss.com>
2017-05-09 01:25:29 +0200

    first working version

--- index.js
+++ index.js
@@ -1,6 +1,4 @@
 const signalkSchema = require('signalk-schema')
-const Bacon = require('baconjs')
-const fs = require('fs')
 var find = require('fs-find')
 , path = process.cwd();
 
@@ -8,7 +6,7 @@ const relevantKeys = Object.keys(signalkSchema.metadata)
 .filter(s => s.indexOf('/vessels/*') >= 0)
 .map(s => s.replace('/vessels/*', '').replace(/\//g, '.').replace(/RegExp/g, '*').substring(1))
 
-var fileList = []
+var fileList = [];
 
 var list = find("settings", function(err, results) {
   if(err) {
@@ -16,9 +14,9 @@ var list = find("settings", function(err, results) {
   }
   for(var item of results) {
     //Only show .json files in enum
-    if(item.file.indexOf('.json') !== -1){fileList.push(item.file)}
+    if(item.file.indexOf('.json') !== -1){fileList.push(item.file);}
   }
-})
+});
 
 var data = function getContent(){
   fs.readFile('settings/aava-file-settings.json', 'utf8', function (err,data) {
@@ -26,29 +24,22 @@ var data = function getContent(){
       return console.log(err);
     }
     console.log(data);
-    return JSON.stringify(data)
+    return JSON.stringify(data);
   });
 
-}
+};
 
 module.exports = function(app) {
-  var plugin = {}
-  var unsubscribes = []
+  var plugin = {};
+  var unsubscribes = [];
 
-  plugin.id = "static-data-edit"
-  plugin.name = "Edit static data for vessel"
-  plugin.description = "A plugin to set vessel info and static values"
-  //Values that don’t change could be in default.json
-  //Values that might change should be visible in the plugin (sail areas, names), updated on ‘submit'
-  //pick and choose json blocks from settings examples (to/from file, N2K, NMEA0183 etc.)
+  plugin.id = "static-data-edit";
+  plugin.name = "Edit static data for vessel";
+  plugin.description = "A plugin to set vessel info and static values";
+
   plugin.schema = {
     type: "object",
     properties: {
-      file: {
-        title: "Settings file to import",
-        type: "string",
-        "enum": fileList
-      },
       saveAs: {
         type: "string",
         title: "Save as",
@@ -57,7 +48,7 @@ module.exports = function(app) {
       "name"  : {
         title: "Vessel name",
         type: "string",
-        default: "Volare"
+        default: ""
       },
       "brand" : {
         title: "Vessel brand",
@@ -72,7 +63,37 @@ module.exports = function(app) {
       "uuid"  : {
         title: "Signal K UUID",
         type: "string",
-        default: ""
+        default: app.signalk.self.uuid
+      },
+      "ws": {
+        title: "websocket",
+        type: "boolean",
+        default: true
+      },
+      "mdns": {
+        title: "mdns",
+        type: "boolean",
+        default: true
+      },
+      "bower": {
+        title: "bower",
+        type: "boolean",
+        default: true
+      },
+      "rest": {
+        title: "rest",
+        type: "boolean",
+        default: true
+      },
+      "tcp": {
+        title: "tcp",
+        type: "boolean",
+        default: true
+      },
+      "nmea-tcp": {
+        title: "nmea_tcp",
+        type: "boolean",
+        default: true
       },
       optional: {
         type: "array",
@@ -81,36 +102,26 @@ module.exports = function(app) {
           title: "data",
           type: "object",
           properties: {
-          "active": {
-            title: "Active",
-            type: "boolean",
-            default: true
-          },
-          "infoType": {
-            title: "value",
-            type: "string",
-            "enum": ["mmsi", "callsign"],
-            enumNames: ["International MMSI number", "VHF call sign"]
-          },
-          "value": {
-            title: "value",
-            type: "string"
+            "infoType": {
+              title: "value",
+              type: "string",
+              "enum": ["mmsi", "callsign"],
+              enumNames: ["International MMSI number", "VHF call sign"]
+            },
+            "value": {
+              title: "value",
+              type: "string"
+            }
           }
         }
-      }
       },
       dimensions: {
         type: "array",
         title: "Optional dimensions",
         items: {
-          title: "Dimensions",
+          title: " ",
           type: "object",
           properties: {
-            "active": {
-              title: "Active",
-              type: "boolean",
-              default: true
-            },
             "dimensionType": {
               title: "dimension",
               type: "string",
@@ -119,6 +130,40 @@ module.exports = function(app) {
             },
             "dimension": {
               title: "value",
+              type: "number"
+            }
+          }
+        }
+      },
+      providers: {
+        type: "array",
+        title: "",
+        items: {
+          title: "Providers",
+          type: "object",
+          properties: {
+            "active": {
+              title: "Active",
+              type: "boolean",
+              default: true
+            },
+            "id": {
+              title: "Provider ID",
+              type: "string"
+            },
+            "type": {
+              title: "Select Provider",
+              type: "number",
+              default: 0,
+              "enum": [0,1,2,3],
+              enumNames: ["NMEA0183 from file (option 1: filename, option 2: throttle rate)", "NMEA0183 from serial (option 1: device, option 2: baudrate)", "N2K from file (option 1: file name, option 2: throttle rate)", "N2K from serial (option 1: device, option 2 not used)"]
+            },
+            "option1": {
+              title: "option 1",
+              type: "string",
+            },
+            "option2": {
+              title: "option 2",
               type: "number"
             }
           }
@@ -135,23 +180,187 @@ module.exports = function(app) {
 
 
   plugin.start = function(options) {
+    var jsonfile = require('jsonfile');
+    var file = options.saveAs;
+    var obj = {
+      "vessel": {
+        "name"  : options.name,
+        "brand" : options.brand,
+        "type"  : options.type,
+        "uuid"  : options.uuid,
+
+      },
+      "interfaces": {
+        "bower": options.bower,
+        "rest": options.rest,
+        "ws": options.ws,
+        "tcp": options.tcp,
+        "nmea-tcp": options.nmea_tcp
+      },
+      "pipedProviders": []
+    };
+    if (options.dimensions) {
+      obj.vessel.dimensions = {};
+      options.dimensions.forEach((item)=>{
+        obj.vessel.dimensions[item.dimensionType] = item.dimension;
+      });
+    }
+
+    options.providers.forEach(function(item){
+      if(item.active){
+        if(item.type === 0){
+          obj.pipedProviders.push(
+            {
+              "id": item.id,
+              "pipeElements": [
+                {
+                  "type": "providers/filestream",
+                  "options": {
+                    "filename": item.option1
+                  },
+                  "optionMappings": [
+                    {
+                      "fromAppProperty": "argv.nmeafilename",
+                      "toOption": "filename"
+                    }
+                  ]
+                },
+                {
+                  "type": "providers/throttle",
+                  "options": {
+                    "rate": item.option2
+                  }
+                },
+                {
+                  "type": "providers/liner"
+                },
+                {
+                  "type": "providers/nmea0183-signalk",
+                  "optionMappings": [
+                    {
+                      "fromAppProperty": "selfId",
+                      "toOption": "selfId"
+                    },
+                    {
+                      "fromAppProperty": "selfType",
+                      "toOption": "selfType"
+                    }
+                  ]
+                }
+              ]
+            }
+          );
+        }
+        if(item.type === 1){
+          obj.pipedProviders.push(
+            {
+              "id": item.id,
+              "pipeElements": [
+                {
+                  "type": "providers/serialport",
+                  "options": {
+                    "device": item.option1,
+                    "baudrate": item.option2
+                  },
+                  "optionMappings": [
+                    {
+                      "fromAppProperty": "argv.nmeadevice",
+                      "toOption": "device"
+                    },
+
+                    {
+                      "fromAppProperty": "argv.nmeabaudrate",
+                      "toOption": "baudrate"
+                    }
+                  ]
+                },
+                {
+                  "type": "providers/nmea0183-signalk",
+                  "optionMappings": [
+                    {
+                      "fromAppProperty": "selfId",
+                      "toOption": "selfId"
+                    },
+                    {
+                      "fromAppProperty": "selfType",
+                      "toOption": "selfType"
+                    }
+                  ]
+                }
+              ]
+            }
+          );
+        }
+        if(item.type === 2){
+          var command = "actisense-serial " + item.option1
+          obj.pipedProviders.push(
+            {
+              "id": item.id,
+              "pipeElements": [
+       {
+         "type": "providers/filestream",
+         "options": {
+           "filename": item.option1
+         },
+         "optionMappings": [
+           {
+             "fromAppProperty": "argv.n2kfilename",
+             "toOption": "filename"
+           }
+         ]
+       },
+       {
+         "type": "providers/liner"
+       },
+       {
+         "type": "providers/n2kAnalyzer"
+       },
+       {
+         "type": "providers/timestamp-throttle"
+       },
+       {
+         "type": "providers/n2k-signalk"
+       }
+    ]
+            }
+          );
+        }
+        if(item.type === 3){
+          var command = "actisense-serial " + item.option1;
+          obj.pipedProviders.push(
+            {
+              "id": item.id,
+              "pipeElements": [{
+                "type": "providers/execute",
+                "options": {
+                  "command": command
+                }
+              }, {
+                "type": "providers/liner"
+              }, {
+                "type": "providers/n2kAnalyzer"
+              }, {
+                "type": "providers/n2k-signalk"
+              }]
+            }
+          );
+        }
+      }
+    });
 
 
-
-    return true
-  }
+    jsonfile.writeFile(file, obj, {spaces: 2}, function (err) {
+      console.error(err);
+    });
+    return true;
+  };
 
   plugin.stop = function() {
-    unsubscribes.forEach(f => f())
-    unsubscribes = []
-  }
-
-  function sendNotificationUpdate(key, zoneIndex, zones) {
-    var value = null
+    unsubscribes.forEach(f => f());
+    unsubscribes = [];
+  };
 
 
-  }
 
-
-  return plugin
-}
+  return plugin;
+};
